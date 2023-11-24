74936cdbe6c5d06564c499c506374b42461b9e35
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-11-24 09:25:51 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ejemplo1sharedPreferencesTema2Pmdm"
+include(":app")
+ 
